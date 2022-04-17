0fde7a4f45ac2aac585c362a7a59a03bc59c39d1
ChmtTnky <okinamatara16@outlook.com>
2022-04-17 01:15:58 -0700

    added comments

--- ImagetoPIM/Program.cs
+++ ImagetoPIM/Program.cs
@@ -18,6 +18,7 @@ if (!File.Exists(imgfile))
 string input;
 bool valid_input = false;
 int bit_depth = 0;
+// the loop is just for input validation
 while (valid_input == false)
 {
     valid_input = true;
@@ -35,18 +36,30 @@ while (valid_input == false)
         valid_input = false;
 }
 
+// based on the bit depth, write the pim file
+// note: pim files use RGBA order, rather than ARGB
+// note: pim alpha values end at 0x80, not 0xFF, so they are all normaized before writing
 switch (bit_depth)
 {
     case 4:
         {
+            // length of palette in bytes
             const int PALETTE_LENGTH = 0x40;
 
+            // make and quantize image file
             Bitmap image = new Bitmap(imgfile);
             image = Octree.Quantize(image, 16);
 
+            // get byte array
+            // 4 bit pim files aren't really designed for odd value pixel counts
+            // so that has to be accounted for by adding 1 more byte if the count is odd
+            // each pixel only takes 4 bits, thus half a byte each
+            // the fraction gets contencated, which is why the extra byte has to be added in odd cases
+            // else, the file would be too short to hold every pixel
             byte[] pimdata = new byte[HEADER_LENGTH + PALETTE_LENGTH + ((image.Width * image.Height) / 2) + ((image.Width * image.Height) % 2)];
 
             // get every color in the source image
+            // this process is slow and should be replaced first
             Color[] palette = Array.Empty<Color>();
             for (int h = 0; h < image.Height; h++)
             {
@@ -55,6 +68,7 @@ switch (bit_depth)
                     bool exists = false;
                     Color pixelcolor = image.GetPixel(w, h);
 
+                    // if the color was already found, move on to the next pixel
                     for (int k = 0; k < palette.Length; k++)
                     {
                         if (pixelcolor == palette[k])
@@ -65,10 +79,13 @@ switch (bit_depth)
                     {
                         continue;
                     }
-                    else
+                    else // add the color to the array
                     {
+                        // not all images have 16 colors in them, which is why the array has no set size
                         Array.Resize(ref palette, palette.Length + 1);
                         palette[palette.Length - 1] = pixelcolor;
+                        // for some reason the quantization wont reduce the image to 16 colors in all cases
+                        // so, the function has to end early if it doesn't
                         if (palette.Length >= 16)
                             break;
                     }
@@ -95,6 +112,7 @@ switch (bit_depth)
                 byte red = palette[i].R;
                 byte green = palette[i].G;
                 byte blue = palette[i].B;
+                // normalize alpha
                 byte alpha = (byte)((palette[i].A + 1) / 2);
 
                 pimdata[(i * 4) + HEADER_LENGTH] = red;
@@ -112,14 +130,16 @@ switch (bit_depth)
                     Color pixelcolor = image.GetPixel(w, h);
                     int index = 0;
 
+                    // iterate through the palette and find the color
                     for (int i = 0; i < palette.Length; i++)
                     {
                         if (pixelcolor == palette[i])
                             index = i;
                     }
 
-                    // if the pixel is even, put in lower bits
+                    // if the pixel index is even, put in lower bits
                     // else put in higher bits
+                    // this is cause 4 bit pim files suck and have a wonky order
                     if (((h * image.Width) + w) % 2 == 0)
                     {
                         pimdata[PALETTE_LENGTH + HEADER_LENGTH + byte_count] += (byte)index;
@@ -146,14 +166,18 @@ switch (bit_depth)
         }
     case 8:
         {
+            // length of palette in bytes
             const int PALETTE_LENGTH = 0x400;
 
+            // make and quantize image
             Bitmap image = new Bitmap(imgfile);
             image = Octree.Quantize(image, 256);
 
+            // make byte array, with one byte per pixel
             byte[] pimdata = new byte[HEADER_LENGTH + PALETTE_LENGTH + image.Width * image.Height];
 
             // get every color in the source image
+            // very slow, especially for this encoding format
             Color[] palette = Array.Empty<Color>();
             for (int h = 0; h < image.Height; h++)
             {
@@ -162,6 +186,8 @@ switch (bit_depth)
                     bool exists = false;
                     Color pixelcolor = image.GetPixel(w, h);
 
+                    // if color is already found, move on to the next pixel
+                    // this is why its slow
                     for (int k = 0; k < palette.Length; k++)
                     {
                         if (pixelcolor == palette[k])
@@ -176,6 +202,7 @@ switch (bit_depth)
                     {
                         Array.Resize(ref palette, palette.Length + 1);
                         palette[palette.Length - 1] = pixelcolor;
+                        // i haven't had issues with the quantization, but this is here just in case
                         if (palette.Length >= 256)
                             break;
                     }
@@ -203,6 +230,7 @@ switch (bit_depth)
                 byte red = palette[i].R;
                 byte green = palette[i].G;
                 byte blue = palette[i].B;
+                // normalize alpha
                 byte alpha = (byte)((palette[i].A + 1) / 2);
 
                 pimdata[(i * 4) + HEADER_LENGTH] = red;
@@ -219,6 +247,7 @@ switch (bit_depth)
                     Color pixelcolor = image.GetPixel(w, h);
                     int index = 0;
 
+                    // find the color of the pixel then write it in
                     for (int i = 0; i < palette.Length; i++)
                     {
                         if (pixelcolor == palette[i])
@@ -246,6 +275,7 @@ switch (bit_depth)
             // does not need to be quantized due to a lack of a palette
             Bitmap image = new Bitmap(imgfile);
 
+            // this format has 4 bytes per pixel
             byte[] pimdata = new byte[HEADER_LENGTH + (4 * image.Width * image.Height)];
 
             // write width and height into header
@@ -258,6 +288,7 @@ switch (bit_depth)
             pimdata[4] = 0x20;
             pimdata[12] = 0x10;
 
+            // for each pixel write the color values in directly
             for (int h = 0; h < image.Height; h++)
             {
                 for (int w = 0; w < image.Width; w++)
@@ -267,6 +298,7 @@ switch (bit_depth)
                     pimdata[HEADER_LENGTH + (4 * ((h * image.Width) + w))] = pixel_color.R;
                     pimdata[HEADER_LENGTH + (4 * ((h * image.Width) + w)) + 1] = pixel_color.G;
                     pimdata[HEADER_LENGTH + (4 * ((h * image.Width) + w)) + 2] = pixel_color.B;
+                    // normalize alpha
                     pimdata[HEADER_LENGTH + (4 * ((h * image.Width) + w)) + 3] = (byte)((pixel_color.A + 1) / 2);
                 }
             }
